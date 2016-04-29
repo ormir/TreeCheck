26e36730c11d8229816a8c7d29968782e80ea7e4
Ormir G <ormirgj@gmail.com>
2016-04-29 16:38:12 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TreeCheck
+//
+//  Created by Ormir G. on 29/04/16.
+//  Copyright © 2016 Ormir Gjurgjej, Nitika Kumar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
